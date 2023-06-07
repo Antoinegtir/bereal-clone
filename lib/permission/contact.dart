ad6aa23ea0c704d02120994ef59ac323c31b8fc1
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-06-07 18:45:13 +0200

    🔐 ask permission to activate notification & access to user contact

---
+++ lib/permission/contact.dart
@@ -0,0 +1,142 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rebeal/styles/color.dart';
+
+import '../animation/animation.dart';
+import 'notification.dart';
+
+class ContactPage extends StatefulWidget {
+  final VoidCallback? loginCallback;
+  ContactPage({Key? key, this.loginCallback}) : super(key: key);
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  List<String> contactEmails = [];
+  @override
+  void initState() {
+    super.initState();
+    ContactsService.getContacts().then((contacts) {
+      for (var contact in contacts) {
+        for (var email in contact.emails!) {
+          contactEmails.add(email.value!);
+        }
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          if (details.delta.dx > 0) {
+            setState(() {
+              Navigator.pop(context);
+            });
+          }
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              actions: [
+                Padding(
+                    padding: EdgeInsets.only(top: 15, right: 10),
+                    child: GestureDetector(
+                        onTap: () {
+                          HapticFeedback.heavyImpact();
+                          Navigator.push(
+                            context,
+                            AwesomePageRoute(
+                              transitionDuration: Duration(milliseconds: 600),
+                              exitPage: widget,
+                              enterPage: NotificationPage(),
+                              transition: ZoomOutSlideTransition(),
+                            ),
+                          );
+                        },
+                        child: Text("Passer",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 61, 61, 61),
+                            ))))
+              ],
+              elevation: 0,
+              title: Image.asset(
+                "assets/rebeals.png",
+                height: 100,
+              ),
+              backgroundColor: Colors.black,
+            ),
+            extendBodyBehindAppBar: true,
+            backgroundColor: Colors.black,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 130,
+                ),
+                Text(
+                  "Trouver tes amis\n",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.w700),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Trouvons qui parmi tes amis utilise\n déjà sur Rebeal.",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+                Expanded(
+                    child: ListView.builder(
+                  itemCount: contactEmails.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                        title: Text(contactEmails[index]),
+                        leading: Icon(CupertinoIcons.profile_circled),
+                        trailing: Container(
+                            width: 120,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                    height: 30,
+                                    width: 90,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: ReBealColor.ReBealDarkGrey,
+                                        borderRadius:
+                                            BorderRadius.circular(90)),
+                                    child: Text(
+                                      "AJOUTER",
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w800),
+                                    )),
+                                Container(
+                                  width: 10,
+                                ),
+                                Icon(
+                                  Icons.close,
+                                  size: 18,
+                                  color: ReBealColor.ReBealLightGrey,
+                                )
+                              ],
+                            )));
+                  },
+                ))
+              ],
+            )));
+  }
+}
---
+++ lib/permission/notification.dart
@@ -0,0 +1,184 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../animation/animation.dart';
+import '../notification/notification.dart';
+
+class NotificationPage extends StatefulWidget {
+  final VoidCallback? loginCallback;
+  NotificationPage({Key? key, this.loginCallback}) : super(key: key);
+
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  Future<void> requestNotificationPermission() async {
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+   
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Sample Notification',
+      'This is a sample notification',
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          'channel_id',
+          'channel_name',
+          'channel_description',
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
+        iOS: IOSNotificationDetails(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          elevation: 0,
+          backgroundColor: Colors.black,
+        ),
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 130,
+            ),
+            Text(
+              "Quand poster ton\nReBeal ?\n",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "La seule façon de savoir quand poster ton\nReBeal est d'activer les notiifcations !",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              height: 30,
+            ),
+            Container(
+              height: 300,
+              width: 250,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 1),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  Text(
+                    "\nMerci d'activer les\n notifications",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "\nToutes les notifications sur ReBeal\nsont silencieuse sauf celle qui\nt'indique quand poster ron BeReal\n une fois par jour.",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300),
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                      onTap: () async {
+                        HapticFeedback.heavyImpact();
+                        FlutterLocalNotificationsPlugin
+                            flutterLocalNotificationsPlugin =
+                            FlutterLocalNotificationsPlugin();
+                        await flutterLocalNotificationsPlugin.show(
+                          0,
+                          'Sample Notification',
+                          'This is a sample notification',
+                          NotificationDetails(
+                            android: AndroidNotificationDetails(
+                              'channel_id',
+                              'channel_name',
+                              'channel_description',
+                              importance: Importance.max,
+                              priority: Priority.high,
+                            ),
+                            iOS: IOSNotificationDetails(),
+                          ),
+                        );
+                        Navigator.push(
+                          context,
+                          AwesomePageRoute(
+                            transitionDuration: Duration(milliseconds: 600),
+                            exitPage: widget,
+                            enterPage: NotifcationTest(),
+                            transition: ZoomOutSlideTransition(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: 260,
+                        height: 40,
+                        color: Color.fromARGB(255, 0, 120, 232),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Autoriser",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                  Container(
+                    height: 10,
+                  ),
+                  Text(
+                    "Autoriser dans le\nRésumé programmé",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 89, 89, 89),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Container(
+                    color: Colors.grey,
+                    height: 0.3,
+                    width: 250,
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Text(
+                    "Refuser",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 89, 89, 89),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
