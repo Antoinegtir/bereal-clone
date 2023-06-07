7958e60bf0029ae9462d05ebed65b67b80ca09e8
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-06-07 18:44:25 +0200

    :art: add style folder that init color palette of BeReal

---
+++ lib/styles/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ReBealColor {
+  static const Color ReBealDarkGrey = Color(0xff1c1c1e);
+  static const Color ReBealLightGrey = Color(0xff99999b);
+}
