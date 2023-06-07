31871b7dd8d8c841d7363f3176a76e56940c1207
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-06-07 19:33:51 +0200

    [SPLASH] add splash file that allow to create a transition if you are log in retrieve homepage or if you are log out retrieve sign up process pages

--- lib/common/splash.dart
+++ lib/common/splash.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:rebeal/auth/name.dart';
 import 'package:rebeal/helper/enum.dart';
 import 'package:rebeal/state/authState.dart';
-import 'package:rebeal/ui/home.dart';
+import 'package:rebeal/pages/home.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
