b4fec76068007fe264c85385f42788d6d04303db
Antoine Gonthier <antoine.gonthier@epitech.eu>
2023-07-07 09:25:55 +0200

    🧪 tests + build JS

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,14 +9,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  
 }
